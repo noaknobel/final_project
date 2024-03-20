eaba3688086c0598092610fee1c4b896deebdbad
Shachar I <shacharisraeli132@gmail.com>
2024-03-20 15:09:43 +0200

    Add typing to node value and fix print

--- src/node.py
+++ src/node.py
@@ -1,14 +1,17 @@
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Union
+
+from math_operator import MathOperator
 
 
 class Node:
-    def __init__(self, value: Any, left: Optional["Node"] = None, right: Optional["Node"] = None) -> None:
+    def __init__(self, value: Union[MathOperator, float, str], left: Optional["Node"] = None, right: Optional["Node"] = None) -> None:
         self.value = value
         self.left = left
         self.right = right
 
     def __str__(self) -> str:
-        return f"Node: (value: {self.value}, left: {self.left}, right: {self.right})"
+        value = self.value.symbol if isinstance(self.value, MathOperator) else self.value
+        return f"Node: (value: {value}, left: {self.left}, right: {self.right})"
 
     def __repr__(self) -> str:
         return f"Node(value={self.value}, left={self.left}, right={self.right})"
